11b323a632a1e11c455ad800a529d9a0bc9ea452
edanurarslan <116951700+edanurarslan@users.noreply.github.com>
2024-08-24 12:24:01 +0300

    first commit

---
+++ Temp/StagingArea/Il2Cpp/il2cppOutput/GoogleMobileAds.Common_CodeGen.c
@@ -0,0 +1,242 @@
+﻿#include "pch-c.h"
+#ifndef _MSC_VER
+# include <alloca.h>
+#else
+# include <malloc.h>
+#endif
+
+
+#include "codegen/il2cpp-codegen-metadata.h"
+
+
+
+
+
+// 0x00000001 System.Void GoogleMobileAds.Common.AdErrorClientEventArgs::.ctor()
+extern void AdErrorClientEventArgs__ctor_mC7A6C70D8C23A82EF4C87E2AC8FA83B86621ADB2 (void);
+// 0x00000002 GoogleMobileAds.Common.IAdErrorClient GoogleMobileAds.Common.AdErrorClientEventArgs::get_AdErrorClient()
+extern void AdErrorClientEventArgs_get_AdErrorClient_mA2821CE27B1C9869E5891C204DC5E462F916285B (void);
+// 0x00000003 System.Void GoogleMobileAds.Common.AdErrorClientEventArgs::set_AdErrorClient(GoogleMobileAds.Common.IAdErrorClient)
+extern void AdErrorClientEventArgs_set_AdErrorClient_m361231E2465DA0BC8D8709F83CA658386A22FF75 (void);
+// 0x00000004 System.Void GoogleMobileAds.Common.AdInspectorErrorClientEventArgs::.ctor()
+extern void AdInspectorErrorClientEventArgs__ctor_mF84E55EF51BB34FCED5F0C85C69734A359B3B2F9 (void);
+// 0x00000005 System.Void GoogleMobileAds.Common.AdInspectorErrorClientEventArgs::set_AdErrorClient(GoogleMobileAds.Common.IAdInspectorErrorClient)
+extern void AdInspectorErrorClientEventArgs_set_AdErrorClient_m0F9F1BAC327F3983447115242D5DC06CA417A1EF (void);
+// 0x00000006 System.Void GoogleMobileAds.Common.AppStateEventClient::.ctor()
+extern void AppStateEventClient__ctor_mCDB33AEEE7C565A6DEEAA6BE7BFCA24D3E923E7B (void);
+// 0x00000007 GoogleMobileAds.Common.AppStateEventClient GoogleMobileAds.Common.AppStateEventClient::get_Instance()
+extern void AppStateEventClient_get_Instance_m46913E98EAED2F8C96B8317683110F7DA9008959 (void);
+// 0x00000008 System.Void GoogleMobileAds.Common.AppStateEventClient::add_AppStateChanged(System.Action`1<GoogleMobileAds.Common.AppState>)
+extern void AppStateEventClient_add_AppStateChanged_mFEBCAC97109E85606A2096B4CC11DA0A933A6947 (void);
+// 0x00000009 System.Void GoogleMobileAds.Common.AppStateEventClient::remove_AppStateChanged(System.Action`1<GoogleMobileAds.Common.AppState>)
+extern void AppStateEventClient_remove_AppStateChanged_m1390FEB91F2264C9C72EAB50EB4313E22C471271 (void);
+// 0x0000000A System.Void GoogleMobileAds.Common.AppStateEventClient::OnApplicationPause(System.Boolean)
+extern void AppStateEventClient_OnApplicationPause_m60E32C6D604465CBDF1D0A26D7D2F10750BDCA33 (void);
+// 0x0000000B System.Void GoogleMobileAds.Common.AppStateEventClient::<AppStateChanged>m__0(GoogleMobileAds.Common.AppState)
+extern void AppStateEventClient_U3CAppStateChangedU3Em__0_m351C92736F5E4A03DB44270F648773D60A8DF3C8 (void);
+// 0x0000000C System.Void GoogleMobileAds.Common.IBannerClient::add_OnAdLoaded(System.EventHandler`1<System.EventArgs>)
+// 0x0000000D System.Void GoogleMobileAds.Common.IBannerClient::remove_OnAdLoaded(System.EventHandler`1<System.EventArgs>)
+// 0x0000000E System.Void GoogleMobileAds.Common.IBannerClient::add_OnAdFailedToLoad(System.EventHandler`1<GoogleMobileAds.Common.LoadAdErrorClientEventArgs>)
+// 0x0000000F System.Void GoogleMobileAds.Common.IBannerClient::remove_OnAdFailedToLoad(System.EventHandler`1<GoogleMobileAds.Common.LoadAdErrorClientEventArgs>)
+// 0x00000010 System.Void GoogleMobileAds.Common.IBannerClient::add_OnAdOpening(System.EventHandler`1<System.EventArgs>)
+// 0x00000011 System.Void GoogleMobileAds.Common.IBannerClient::remove_OnAdOpening(System.EventHandler`1<System.EventArgs>)
+// 0x00000012 System.Void GoogleMobileAds.Common.IBannerClient::add_OnAdClosed(System.EventHandler`1<System.EventArgs>)
+// 0x00000013 System.Void GoogleMobileAds.Common.IBannerClient::remove_OnAdClosed(System.EventHandler`1<System.EventArgs>)
+// 0x00000014 System.Void GoogleMobileAds.Common.IBannerClient::add_OnPaidEvent(System.EventHandler`1<GoogleMobileAds.Api.AdValueEventArgs>)
+// 0x00000015 System.Void GoogleMobileAds.Common.IBannerClient::remove_OnPaidEvent(System.EventHandler`1<GoogleMobileAds.Api.AdValueEventArgs>)
+// 0x00000016 System.Void GoogleMobileAds.Common.IBannerClient::CreateBannerView(System.String,GoogleMobileAds.Api.AdSize,GoogleMobileAds.Api.AdPosition)
+// 0x00000017 System.Void GoogleMobileAds.Common.IBannerClient::LoadAd(GoogleMobileAds.Api.AdRequest)
+// 0x00000018 GoogleMobileAds.Common.IBannerClient GoogleMobileAds.IClientFactory::BuildBannerClient()
+// 0x00000019 GoogleMobileAds.Common.IInterstitialClient GoogleMobileAds.IClientFactory::BuildInterstitialClient()
+// 0x0000001A GoogleMobileAds.Common.IMobileAdsClient GoogleMobileAds.IClientFactory::MobileAdsInstance()
+// 0x0000001B System.Void GoogleMobileAds.Common.IInterstitialClient::add_OnAdLoaded(System.EventHandler`1<System.EventArgs>)
+// 0x0000001C System.Void GoogleMobileAds.Common.IInterstitialClient::remove_OnAdLoaded(System.EventHandler`1<System.EventArgs>)
+// 0x0000001D System.Void GoogleMobileAds.Common.IInterstitialClient::add_OnAdFailedToLoad(System.EventHandler`1<GoogleMobileAds.Common.LoadAdErrorClientEventArgs>)
+// 0x0000001E System.Void GoogleMobileAds.Common.IInterstitialClient::remove_OnAdFailedToLoad(System.EventHandler`1<GoogleMobileAds.Common.LoadAdErrorClientEventArgs>)
+// 0x0000001F System.Void GoogleMobileAds.Common.IInterstitialClient::add_OnPaidEvent(System.EventHandler`1<GoogleMobileAds.Api.AdValueEventArgs>)
+// 0x00000020 System.Void GoogleMobileAds.Common.IInterstitialClient::remove_OnPaidEvent(System.EventHandler`1<GoogleMobileAds.Api.AdValueEventArgs>)
+// 0x00000021 System.Void GoogleMobileAds.Common.IInterstitialClient::add_OnAdFailedToPresentFullScreenContent(System.EventHandler`1<GoogleMobileAds.Common.AdErrorClientEventArgs>)
+// 0x00000022 System.Void GoogleMobileAds.Common.IInterstitialClient::remove_OnAdFailedToPresentFullScreenContent(System.EventHandler`1<GoogleMobileAds.Common.AdErrorClientEventArgs>)
+// 0x00000023 System.Void GoogleMobileAds.Common.IInterstitialClient::add_OnAdDidPresentFullScreenContent(System.EventHandler`1<System.EventArgs>)
+// 0x00000024 System.Void GoogleMobileAds.Common.IInterstitialClient::remove_OnAdDidPresentFullScreenContent(System.EventHandler`1<System.EventArgs>)
+// 0x00000025 System.Void GoogleMobileAds.Common.IInterstitialClient::add_OnAdDidDismissFullScreenContent(System.EventHandler`1<System.EventArgs>)
+// 0x00000026 System.Void GoogleMobileAds.Common.IInterstitialClient::remove_OnAdDidDismissFullScreenContent(System.EventHandler`1<System.EventArgs>)
+// 0x00000027 System.Void GoogleMobileAds.Common.IInterstitialClient::add_OnAdDidRecordImpression(System.EventHandler`1<System.EventArgs>)
+// 0x00000028 System.Void GoogleMobileAds.Common.IInterstitialClient::remove_OnAdDidRecordImpression(System.EventHandler`1<System.EventArgs>)
+// 0x00000029 System.Void GoogleMobileAds.Common.IInterstitialClient::CreateInterstitialAd()
+// 0x0000002A System.Void GoogleMobileAds.Common.IInterstitialClient::LoadAd(System.String,GoogleMobileAds.Api.AdRequest)
+// 0x0000002B System.Void GoogleMobileAds.Common.IInterstitialClient::Show()
+// 0x0000002C System.Void GoogleMobileAds.Common.IMobileAdsClient::Initialize(System.Action`1<GoogleMobileAds.Common.IInitializationStatusClient>)
+// 0x0000002D System.Void GoogleMobileAds.Common.LoadAdErrorClientEventArgs::.ctor()
+extern void LoadAdErrorClientEventArgs__ctor_m8FF3631839F06576CB7F9492ACB5EF61BECFD52E (void);
+// 0x0000002E GoogleMobileAds.Common.ILoadAdErrorClient GoogleMobileAds.Common.LoadAdErrorClientEventArgs::get_LoadAdErrorClient()
+extern void LoadAdErrorClientEventArgs_get_LoadAdErrorClient_m47E6D04DEF85BA69C87809F85E24A1698CDF6820 (void);
+// 0x0000002F System.Void GoogleMobileAds.Common.LoadAdErrorClientEventArgs::set_LoadAdErrorClient(GoogleMobileAds.Common.ILoadAdErrorClient)
+extern void LoadAdErrorClientEventArgs_set_LoadAdErrorClient_m4549F54FF71F183C081FA18053FFCA75F6F3001E (void);
+// 0x00000030 System.Void GoogleMobileAds.Common.MobileAdsEventExecutor::.ctor()
+extern void MobileAdsEventExecutor__ctor_mE40B013A9F35241AE1CB87F559568279C9148E4A (void);
+// 0x00000031 System.Void GoogleMobileAds.Common.MobileAdsEventExecutor::Initialize()
+extern void MobileAdsEventExecutor_Initialize_m2B5E166E331701205FF86B14ADE8DE0EBB49366C (void);
+// 0x00000032 System.Boolean GoogleMobileAds.Common.MobileAdsEventExecutor::IsActive()
+extern void MobileAdsEventExecutor_IsActive_m13D3462AC66EF7CF8316818BC571587A5E4F0354 (void);
+// 0x00000033 System.Void GoogleMobileAds.Common.MobileAdsEventExecutor::Awake()
+extern void MobileAdsEventExecutor_Awake_m5F1BEBCEC27717844EBFD70060A6FFB21983A7C2 (void);
+// 0x00000034 System.Void GoogleMobileAds.Common.MobileAdsEventExecutor::ExecuteInUpdate(System.Action)
+extern void MobileAdsEventExecutor_ExecuteInUpdate_m26262C3C3D9064856311BAFC0DFF0BC77D9A2668 (void);
+// 0x00000035 System.Void GoogleMobileAds.Common.MobileAdsEventExecutor::InvokeInUpdate(UnityEngine.Events.UnityEvent)
+extern void MobileAdsEventExecutor_InvokeInUpdate_m55CFDDB646C8849BF389A2631CCC5C24665C3185 (void);
+// 0x00000036 System.Void GoogleMobileAds.Common.MobileAdsEventExecutor::Update()
+extern void MobileAdsEventExecutor_Update_mC6BB433B083D86A901CD570E3524925556441714 (void);
+// 0x00000037 System.Void GoogleMobileAds.Common.MobileAdsEventExecutor::OnDisable()
+extern void MobileAdsEventExecutor_OnDisable_m95342A1E456BB8D6C1ACF39115CD965B559F5813 (void);
+// 0x00000038 System.Void GoogleMobileAds.Common.MobileAdsEventExecutor::.cctor()
+extern void MobileAdsEventExecutor__cctor_m6ED53349880E23F164D760AF60D19AB02BE936C2 (void);
+// 0x00000039 System.Void GoogleMobileAds.Common.MobileAdsEventExecutor/<InvokeInUpdate>c__AnonStorey0::.ctor()
+extern void U3CInvokeInUpdateU3Ec__AnonStorey0__ctor_m939C4B1497C4B851341CE4DDAE4DF7AA9CB138CA (void);
+// 0x0000003A System.Void GoogleMobileAds.Common.MobileAdsEventExecutor/<InvokeInUpdate>c__AnonStorey0::<>m__0()
+extern void U3CInvokeInUpdateU3Ec__AnonStorey0_U3CU3Em__0_mAB06C99EC6978DEE16E8A66F4C5BB31C570EA368 (void);
+static Il2CppMethodPointer s_methodPointers[58] = 
+{
+	AdErrorClientEventArgs__ctor_mC7A6C70D8C23A82EF4C87E2AC8FA83B86621ADB2,
+	AdErrorClientEventArgs_get_AdErrorClient_mA2821CE27B1C9869E5891C204DC5E462F916285B,
+	AdErrorClientEventArgs_set_AdErrorClient_m361231E2465DA0BC8D8709F83CA658386A22FF75,
+	AdInspectorErrorClientEventArgs__ctor_mF84E55EF51BB34FCED5F0C85C69734A359B3B2F9,
+	AdInspectorErrorClientEventArgs_set_AdErrorClient_m0F9F1BAC327F3983447115242D5DC06CA417A1EF,
+	AppStateEventClient__ctor_mCDB33AEEE7C565A6DEEAA6BE7BFCA24D3E923E7B,
+	AppStateEventClient_get_Instance_m46913E98EAED2F8C96B8317683110F7DA9008959,
+	AppStateEventClient_add_AppStateChanged_mFEBCAC97109E85606A2096B4CC11DA0A933A6947,
+	AppStateEventClient_remove_AppStateChanged_m1390FEB91F2264C9C72EAB50EB4313E22C471271,
+	AppStateEventClient_OnApplicationPause_m60E32C6D604465CBDF1D0A26D7D2F10750BDCA33,
+	AppStateEventClient_U3CAppStateChangedU3Em__0_m351C92736F5E4A03DB44270F648773D60A8DF3C8,
+	NULL,
+	NULL,
+	NULL,
+	NULL,
+	NULL,
+	NULL,
+	NULL,
+	NULL,
+	NULL,
+	NULL,
+	NULL,
+	NULL,
+	NULL,
+	NULL,
+	NULL,
+	NULL,
+	NULL,
+	NULL,
+	NULL,
+	NULL,
+	NULL,
+	NULL,
+	NULL,
+	NULL,
+	NULL,
+	NULL,
+	NULL,
+	NULL,
+	NULL,
+	NULL,
+	NULL,
+	NULL,
+	NULL,
+	LoadAdErrorClientEventArgs__ctor_m8FF3631839F06576CB7F9492ACB5EF61BECFD52E,
+	LoadAdErrorClientEventArgs_get_LoadAdErrorClient_m47E6D04DEF85BA69C87809F85E24A1698CDF6820,
+	LoadAdErrorClientEventArgs_set_LoadAdErrorClient_m4549F54FF71F183C081FA18053FFCA75F6F3001E,
+	MobileAdsEventExecutor__ctor_mE40B013A9F35241AE1CB87F559568279C9148E4A,
+	MobileAdsEventExecutor_Initialize_m2B5E166E331701205FF86B14ADE8DE0EBB49366C,
+	MobileAdsEventExecutor_IsActive_m13D3462AC66EF7CF8316818BC571587A5E4F0354,
+	MobileAdsEventExecutor_Awake_m5F1BEBCEC27717844EBFD70060A6FFB21983A7C2,
+	MobileAdsEventExecutor_ExecuteInUpdate_m26262C3C3D9064856311BAFC0DFF0BC77D9A2668,
+	MobileAdsEventExecutor_InvokeInUpdate_m55CFDDB646C8849BF389A2631CCC5C24665C3185,
+	MobileAdsEventExecutor_Update_mC6BB433B083D86A901CD570E3524925556441714,
+	MobileAdsEventExecutor_OnDisable_m95342A1E456BB8D6C1ACF39115CD965B559F5813,
+	MobileAdsEventExecutor__cctor_m6ED53349880E23F164D760AF60D19AB02BE936C2,
+	U3CInvokeInUpdateU3Ec__AnonStorey0__ctor_m939C4B1497C4B851341CE4DDAE4DF7AA9CB138CA,
+	U3CInvokeInUpdateU3Ec__AnonStorey0_U3CU3Em__0_mAB06C99EC6978DEE16E8A66F4C5BB31C570EA368,
+};
+static const int32_t s_InvokerIndices[58] = 
+{
+	1614,
+	1569,
+	1352,
+	1614,
+	1352,
+	1614,
+	2650,
+	1352,
+	1352,
+	1309,
+	2622,
+	1352,
+	1352,
+	1352,
+	1352,
+	1352,
+	1352,
+	1352,
+	1352,
+	1352,
+	1352,
+	508,
+	1352,
+	1569,
+	1569,
+	1569,
+	1352,
+	1352,
+	1352,
+	1352,
+	1352,
+	1352,
+	1352,
+	1352,
+	1352,
+	1352,
+	1352,
+	1352,
+	1352,
+	1352,
+	1614,
+	810,
+	1614,
+	1352,
+	1614,
+	1569,
+	1352,
+	1614,
+	2666,
+	2637,
+	1614,
+	2625,
+	2625,
+	1614,
+	1614,
+	2666,
+	1614,
+	1614,
+};
+extern const CustomAttributesCacheGenerator g_GoogleMobileAds_Common_AttributeGenerators[];
+IL2CPP_EXTERN_C const Il2CppCodeGenModule g_GoogleMobileAds_Common_CodeGenModule;
+const Il2CppCodeGenModule g_GoogleMobileAds_Common_CodeGenModule = 
+{
+	"GoogleMobileAds.Common.dll",
+	58,
+	s_methodPointers,
+	0,
+	NULL,
+	s_InvokerIndices,
+	0,
+	NULL,
+	0,
+	NULL,
+	0,
+	NULL,
+	NULL,
+	g_GoogleMobileAds_Common_AttributeGenerators,
+	NULL, // module initializer,
+	NULL,
+	NULL,
+	NULL,
+};
